10d2529cab41e3bdea696b8666138cd64ef61d08
QuocAnh171 <74640069+QuocAnh171@users.noreply.github.com>
2020-11-20 17:34:20 +0700

    Add files via upload

---
+++ requests/dev1.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+login = "http://45.79.43.178/source_carts/wordpress/wp-login.php"
+url = "http://45.79.43.178/source_carts/wordpress/wp-admin"
+username = 'admin'
+password = '123456aA'
+key = {'log':username,'pwd':password,'wp-submit':'Log in',
+        'redirect_to':url,'testcokie':'1'}
+res = requests.post(login,data=key)
+print(res.text)
+
